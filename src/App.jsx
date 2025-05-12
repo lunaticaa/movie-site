88e69aa7ff036cb62533b19c4d1e0fdf4936576b
amir830047agh@gmail.com <amir830047agh@gmail.com>
2025-05-12 21:00:19 +0330

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './index.css'
 const Card = ({ title }) => {
+  const [count, setCount] = useState(0)
   const [hasLiked, setHasLiked] = useState(false);
+  useEffect(effect: () => {
+    console.log(`${title} has been liked: ${hasLiked}`);
 
+  })
   return (
-    <div className="card">
+    <div className="card" onClick={() => { setCount(value: (prevState) => prevState + 1) }}>
       <h2>{title}</h2>
-      <button onClick={() => setHasLiked(true)}>{hasLiked ? 'Liked' : 'Like'}</button>
+      <button onClick={() => setHasLiked(!hasLiked)}>{hasLiked ? '❤️' : '🤍'}</button>
     </div>
   )
 }
