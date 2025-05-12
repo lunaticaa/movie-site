8a837df3497cac72da501d338fd2c01a1d9cc9f1
amir830047agh@gmail.com <amir830047agh@gmail.com>
2025-05-12 20:40:28 +0330

    operator with state

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { useState } from 'react'
 import './index.css'
 const Card = ({ title }) => {
+  const [hasLiked, setHasLiked] = useState(false);
+
   return (
     <div className="card">
       <h2>{title}</h2>
-      <button onClick={() => setHasLiked(true)}>Like</button>
+      <button onClick={() => setHasLiked(true)}>{hasLiked ? 'Liked' : 'Like'}</button>
     </div>
   )
 }
 const App = () => {
-  const [hasLiked, setHasLiked] = useState(initialState: 'false')
   return (
     <div className='card-container'>
       <Card title="star wars" />
