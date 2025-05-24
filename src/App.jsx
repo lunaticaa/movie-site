eb59d8f54a60b687073beefdf73dd78b8c5131db
amir830047agh@gmail.com <amir830047agh@gmail.com>
2025-05-24 13:46:45 +0330

    adding header

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,43 @@
-import { useEffect, useState } from 'react'
+// import { useEffect, useState } from 'react'
 import './index.css'
-const Card = ({ title }) => {
-  const [count, setCount] = useState(0)
-  const [hasLiked, setHasLiked] = useState(false);
-  useEffect(() => {
-    console.log(`${title} has been liked: ${hasLiked}`)
-  }, [hasLiked]);
+// const Card = ({ title }) => {
+//   const [count, setCount] = useState(0)
+//   const [hasLiked, setHasLiked] = useState(false);
+//   useEffect(() => {
+//     console.log(`${title} has been liked: ${hasLiked}`)
+//   }, [hasLiked]);
 
-  return (
-    <div className="card" onClick={() => setCount(count + 1)}>
-      <h2>{title} <br />{count || null}</h2>
-      <button onClick={() => setHasLiked(!hasLiked)}>{hasLiked ? '❤️' : '🤍'}</button>
-    </div>
-  )
-}
-const App = () => {
-  return (
-    <div className='card-container'>
-      <Card title="star wars" />
-      <Card title="avatars" />
-      <Card title="lion" />
+//   return (
+//     <div className="card" onClick={() => setCount(count + 1)}>
+//       <h2>{title} <br />{count || null}</h2>
+//       <button onClick={() => setHasLiked(!hasLiked)}>{hasLiked ? '❤️' : '🤍'}</button>
+//     </div>
+//   )
+// }
+// const App = () => {
+//   return (
+//     <div className='card-container'>
+//       <Card title="star wars" />
+//       <Card title="avatars" />
+//       <Card title="lion" />
 
-    </div>
+//     </div>
+//   )
+// }
+
+// export default App
+import React from 'react'
+
+function App() {
+  return (
+    <main>
+      <div className='pattern' />
+      <div className='wrapper'>
+        <header>
+          <h1>Find <span className='text-gradient'>Movies</span> With Search</h1>
+        </header>
+      </div>
+    </main>
   )
 }
 
